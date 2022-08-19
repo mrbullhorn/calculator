910cbd5b2ecb798188f7330472a44bcdd86c32d6
Hans Ellis <bullhorn@gmail.com>
2022-08-18 21:53:36 -0700

    fix error in divison

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function multiply(number5, number6) {
   }
 
 function divide(number7, number8) {
-  return number7, number8;
+  return number7 / number8;
 }
 
 //user interface logic
@@ -25,12 +25,12 @@ const number6 = parseInt(prompt("Enter another number"))
 const number7 = parseInt(prompt("Enter a number:"));
 const number8 = parseInt(prompt("Enter another number"))
 
-//window.alert(number1 + "+" + number2 + "=" + add(number1, number2));
-//window.alert(number2 + "-" + number3 + "=" + subtract(number3, number4));
-//window.alert(number5 + "*" + number6 + "=" + multiply(number5, number6));
-//window.alert(number7 + "/" + number8 + "=" + divide(number7, number8));
+window.alert(number1 + "+" + number2 + "=" + add(number1, number2));
+window.alert(number2 + "-" + number3 + "=" + subtract(number3, number4));
+window.alert(number5 + "*" + number6 + "=" + multiply(number5, number6));
+window.alert(number7 + "/" + number8 + "=" + divide(number7, number8));
 
-window.alert("quack")
+
 
 
 
